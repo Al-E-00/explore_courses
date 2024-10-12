f4de05de121b655e55b295adf73cdf26f50fee81
Alessio <alessio.mazzella@siren.io>
2024-10-12 15:04:37 +0200

    Added shadcn

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -2,7 +2,7 @@ import { CourseProps } from "../types/types";
 
 export default function CourseCard({ course }: CourseProps) {
   return (
-    <article className="p-4 border-2 border-sky-200 rounded-lg">
+    <article className="p-4 border-2 border-sky-200 rounded-lg hover:border-sky-300 hover:bg-slate-100 hover:cursor-pointer	 transition-all ">
       <header>
         <h2 className="font-bold">{course.title || "Untitled Course"}</h2>
         <p>{course.description || "No description available."}</p>
