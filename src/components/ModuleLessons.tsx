db82f377d37c34f409ee728efe8ec71847660a47
Alessio <alessio.mazzella@siren.io>
2024-10-13 20:18:25 +0200

    Recommitted functions (VSCODE BUG)

---
+++ src/components/ModuleLessons.tsx
@@ -0,0 +1,46 @@
+import { Lesson } from '../types/types';
+import DisplayItem from './ui/DisplayItem';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+import VisualizeLesson from './VisualizeLesson';
+
+type ModuleLessonsProps = {
+  lesson: Lesson;
+};
+
+export default function ModuleLessons({ lesson }: ModuleLessonsProps) {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <DisplayItem title={lesson.title} description={lesson.description}>
+          <ul className="flex flex-col justify-center gap-2 py-3 md:flex-row">
+            {lesson.topics.map((topic) => (
+              <li
+                className="grow-0 rounded-lg border-2 border-violet-200 bg-gray-50 px-1 py-1 text-xs font-semibold text-gray-800 lg:text-sm"
+                key={topic}
+              >
+                {topic}
+              </li>
+            ))}
+          </ul>
+        </DisplayItem>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{lesson.title}</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          <VisualizeLesson lessonContent={lesson.content} />
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+}
