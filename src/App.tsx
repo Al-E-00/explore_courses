d1021815032f1a590d2f0208573af7a8267de8d6
Alessio <alessio.mazzella@siren.io>
2024-10-16 21:22:24 +0200

    UI fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MainMenu from './components/MainMenu';
 
 function App() {
   return (
-    <div className="px-10 py-6 md:px-20 md:py-10 lg:px-36 lg:py-14 xl:px-44 2xl:px-72">
+    <div className="px-10 py-6 md:px-20 md:py-10 lg:px-36 lg:py-14 xl:px-44 2xl:px-80">
       <MainMenu />
     </div>
   );
