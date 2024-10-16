5164fcf82afb32a46f08cdde04f2c8ace9a02a87
Alessio <alessio.mazzella@siren.io>
2024-10-16 21:20:10 +0200

    Updated padding and margin for App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MainMenu from './components/MainMenu';
 
 function App() {
   return (
-    <div className="px-20 py-16 md:px-40 lg:px-64 xl:px-72 2xl:px-96">
+    <div className="px-10 py-6 md:px-20 md:py-10 lg:px-36 lg:py-14 xl:px-44 2xl:px-72">
       <MainMenu />
     </div>
   );
